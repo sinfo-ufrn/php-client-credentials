680ec763e2286ea2cfa5b8c69bceb6801a7a08ee
Jean Guerethes <guerethes@info.ufrn.br>
2017-09-21 16:01:18 -0300

    Exemplo da login e consulta utilizando PHP.

---
+++ exemplo.php
@@ -0,0 +1,65 @@
+<?php
+
+$x-api-key = ""
+
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://apitestes.info.ufrn.br/authz-server/oauth/token?client_id=<client_id>&client_secret=<client_secret>&grant_type=client_credentials",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "POST"
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+
+echo("<br /><br />");
+
+#Pegando as informações do Json retornado
+$jsonData = json_decode($response, true);
+
+$token = $jsonData['access_token'];
+$token_type = $jsonData['token_type'];
+
+
+#Inicializando a consulta de teste.
+$curlConsulta = curl_init();
+
+curl_setopt_array($curlConsulta, array(
+  CURLOPT_URL => "https://apitestes.info.ufrn.br/curso/<Versao>/modalidades-educacao",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+   CURLOPT_HTTPHEADER => array(
+    "Authorization: " . $token_type . " " . $token,
+    "x-api-key: " . $x-api-key
+  ),
+));
+
+$response = curl_exec($curlConsulta);
+$err = curl_error($curlConsulta);
+
+curl_close($curlConsulta);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+
+?>
